8ed08b5cf9bb15c3f03e1c03d6166305478af76b
JADDOU3 <22the7@gmail.com>
2025-06-25 05:35:24 +0300

    add account settings card

---
+++ src/components/AccountSettings.jsx
@@ -0,0 +1,41 @@
+import '../styles/AccountSettings.css'
+
+function AccountSettings() {
+    const user = {email:"meow@gmail.com"};
+   return(
+       <div className="profile-card">
+           <div className="profile-header">
+                <h2>Account Settings</h2>
+           </div>
+           <div className="details-section">
+               <h2>Account Email</h2>
+               <p>This email is associated with your Learnly account</p>
+               <input type="email" placeholder={user.email} disabled />
+           </div>
+           <div className="details-section">
+               <h2>Change Password</h2>
+               <p>Please enter your new password</p>
+               <input type="password" placeholder="Change Password" />
+               <button>Change Password</button>
+           </div>
+           <div className="danger-zone">
+               <h2>Danger Zone</h2>
+               <div className="danger-zone-card">
+                   <p>Deleting your account will:</p>
+                   <ul>
+                       <li>Permanently delete your profile, along with your authentication association</li>
+                       <li>Permanently delete all your content</li>
+                   </ul>
+                   <p>
+                       Important: deleting your account is unrecoverable and cannot be undone. feel free to
+                       <br />
+                        contact support with any questions
+                   </p>
+                   <button>Delete Account</button>
+               </div>
+           </div>
+       </div>
+   )
+}
+
+export default AccountSettings
