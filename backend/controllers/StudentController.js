dc763fa6e6be0a53053a5d9ad971bf85debd3483
JADDOU3 <22the7@gmail.com>
2025-07-04 20:18:49 +0300

    implement middleware with jwt

--- backend/controllers/StudentController.js
+++ backend/controllers/StudentController.js
@@ -1,10 +1,11 @@
 import Student from '../models/Student.js';
 import HTTP_STATUS from "../utils/respones.js";
-
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
 
 const getStudent = async (req, res) => {
     try{
-        const student = await Student.findById(req.params.id);
+        const student = await Student.findById(req.user.id);
         if(!student){
             res.status(HTTP_STATUS.NOT_FOUND).json({msg: "Student not found"})
         }
@@ -15,20 +16,23 @@ const getStudent = async (req, res) => {
     }
 };
 
-const postStudent = async (req, res) => {
+const register = async (req, res) => {
     const student = req.body;
 
     if (!student.name || !student.email || !student.password || !student.DateOfBirth) {
         return res.status(HTTP_STATUS.FORBIDDEN).json({msg: "Please provide all the required fields"})
     }
 
-    const newStudent = new Student(student);
     try {
+
         const email = student.email;
         const exist = await Student.findOne({email});
         if(exist){
             return res.status(HTTP_STATUS.BAD_REQUEST).json({msg: "Account already exists"})
         }
+
+        student.password = await bcrypt.hash(student.password, 10);
+        const newStudent = new Student(student);
         await newStudent.save();
         res.status(HTTP_STATUS.CREATED).json("Account created successfully");
     } catch (error) {
@@ -40,7 +44,7 @@ const postStudent = async (req, res) => {
 const updateStudent = async (req, res) => {
     const newstudent = req.body;
     try{
-        const student = await Student.findById(req.params.id);
+        const student = await Student.findById( req.user.id );
         if(!student){
             return res.status(HTTP_STATUS.NOT_FOUND).json({msg: "Student not found"})
         }
@@ -48,7 +52,7 @@ const updateStudent = async (req, res) => {
             student.name = newstudent.name;
         }
         if(newstudent.password){
-            student.password = newstudent.password;
+            student.password = await bcrypt.hash(newstudent.password, 10);
         }
         if(newstudent.image){
             student.image = newstudent.image;
@@ -56,7 +60,6 @@ const updateStudent = async (req, res) => {
         if(newstudent.DateOfBirth){
             student.DateOfBirth = newstudent.DateOfBirth;
         }
-
         await student.save();
         res.status(HTTP_STATUS.OK).json("Student updated successfully");
     }
@@ -67,7 +70,7 @@ const updateStudent = async (req, res) => {
 
 const deleteStudent = async (req, res) => {
     try{
-        const student = await Student.findById(req.params.id);
+        const student = await Student.findById(req.user.id);
         if(!student){
             return res.status(HTTP_STATUS.NOT_FOUND).json({msg: "Student not found"})
         }
@@ -80,11 +83,37 @@ const deleteStudent = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(HTTP_STATUS.BAD_REQUEST).json({msg: "Please provide all the required fields"})
+    }
+    try{
+        const user = await Student.findOne({email});
+        if(!user){
+            return res.status(HTTP_STATUS.NOT_FOUND).json({msg: "User not found"})
+        }
+
+        const validatePassword  = await bcrypt.compare( password , user.password );
+        if (!validatePassword) {
+            return res.status(HTTP_STATUS.UNAUTHORIZED).json({msg: "Invalid password"})
+        }
+
+        const token = jwt.sign({id: user.id} , process.env.ACCESS_TOKEN_SECRET)
+
+        res.status(HTTP_STATUS.OK).json({token})
+    }
+    catch (error){
+        res.status(HTTP_STATUS.SERVER_ERROR).json({msg: error.message})
+    }
+}
+
 const StudentController = {
     getStudent,
-    postStudent,
+    register,
     updateStudent,
-    deleteStudent
+    deleteStudent,
+    login,
 }
 
 export default StudentController;
--- backend/routes/StudentRoute.js
+++ backend/routes/StudentRoute.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import StudentController from '../controllers/StudentController.js';
+import authenticate from "../middleware/Authenticate.js";
 
 const router = express.Router();
 
-router.post('/', StudentController.postStudent);
-router.put('/:id', StudentController.updateStudent);
-router.delete('/:id', StudentController.deleteStudent);
-router.get('/:id', StudentController.getStudent);
+router.post('/register', StudentController.register);
+router.post('/login'  ,StudentController.login);
+
+router.put('/me', authenticate ,  StudentController.updateStudent);
+router.delete('/me', authenticate , StudentController.deleteStudent);
+router.get('/me', authenticate , StudentController.getStudent);
 
 export default router;
