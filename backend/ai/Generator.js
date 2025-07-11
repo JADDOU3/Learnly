be8fc71b2b0b463e387351589a8a6b9de16fab0f
JADDOU3 <22the7@gmail.com>
2025-07-11 20:07:11 +0300

    implement Generator

---
+++ backend/ai/Generator.js
@@ -0,0 +1,41 @@
+import LEARNING_STYLES from "../utils/LearningStyles.js";
+
+const Generator = async (trends , aiSettings , learningStyle) => {
+    const activity = [];
+    const intensityMap  = {
+        low: 30,
+        mid: 60,
+        high: 90,
+    };
+
+    const duration = intensityMap [aiSettings.planIntensity] || 60;
+
+    const methods = LEARNING_STYLES[learningStyle];
+
+    const weakSubjects = Object.keys(trends).filter(s => trends[s].weak);
+
+    if(weakSubjects.length === 0){
+        activity.push({
+            subject: "General",
+            duration,
+            method: methods[0],
+            resources: [],
+        })
+        return activity;
+    }
+
+    weakSubjects.forEach(s => {
+        methods.forEach(m => {
+            activity.push({
+                subject: s,
+                duration,
+                method: m,
+                resources: [`https://resources.example.com/${s.toLowerCase()}`],
+            })
+        })
+    });
+
+    return activity;
+}
+
+export default Generator
